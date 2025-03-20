80d87094a9718ead500ae86da8af88820e67ba80
Hannes Stauss <hannes.stauss@htldornbirn.at>
2025-03-20 12:24:24 +0100

    initial sync

---
+++ cmd/sync/sync.go
@@ -0,0 +1,131 @@
+package sync
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/cli/cli/v2/pkg/cmdutil"
+	"github.com/cli/go-gh"
+	"github.com/github/gh-classroom/cmd/gh-classroom/shared"
+	"github.com/scalarion/gh-crm/cmd/clone/utils"
+	"github.com/scalarion/gh-crm/pkg/crm"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdSyncs(f *cmdutil.Factory) *cobra.Command {
+	var aId int
+	var isAssignmentFolder bool
+	var verbose bool
+
+	cmd := &cobra.Command{
+		Use:   "clone",
+		Short: "Clone student repos for an assignment",
+		Long: heredoc.Doc(`Clone student repos for an assignment.
+
+		The student repos are cloned into the current directory in a directory named after the assignment. 
+		If the directory does not exists, it will be created.
+		`),
+		Run: func(cmd *cobra.Command, args []string) {
+			client, err := gh.RESTClient(nil)
+			if err != nil {
+				crm.Fatal(err)
+			}
+
+			c, err := crm.LoadClassroom()
+			if err != nil {
+				crm.Fatal(err)
+			}
+
+			if isAssignmentFolder, err = crm.IsAssignmentFolder(); err == nil && isAssignmentFolder {
+				a, err := crm.LoadAssignment()
+				if err != nil {
+					crm.Fatal(err)
+				}
+				aId = a.Id
+			}
+			if err != nil {
+				crm.Fatal(err)
+			}
+
+			if aId == 0 {
+				a, err := shared.PromptForAssignment(client, c.Classroom.Id)
+				if err != nil {
+					crm.Fatal(err)
+				}
+
+				aId = a.Id
+			}
+
+			acceptedAssignmentList, err := shared.ListAllAcceptedAssignments(client, aId, 15)
+			if err != nil {
+				crm.Fatal(err)
+			}
+
+			assignment := acceptedAssignmentList.Assignment
+			var assignmentPath string
+			if isAssignmentFolder {
+				assignmentPath, err = os.Getwd()
+			} else {
+				assignmentPath, err = filepath.Abs(assignment.Slug)
+			}
+			if err != nil {
+				fmt.Println("Error getting absolute path for directory: ", err)
+				return
+			}
+
+			if !isAssignmentFolder {
+				if _, err := os.Stat(assignmentPath); os.IsNotExist(err) {
+					fmt.Println("Creating directory: ", assignmentPath)
+					err = os.MkdirAll(assignmentPath, 0755)
+					if err != nil {
+						crm.Fatal(err)
+					}
+				}
+
+				a := crm.NewAssignment()
+				a.Set(assignment.Id, assignment.Slug)
+				err = a.Save(assignmentPath)
+				if err != nil {
+					crm.Fatal(err)
+				}
+			}
+
+			totalCloned := 0
+			cloneErrors := []string{}
+			for _, acceptedAssignment := range acceptedAssignmentList.AcceptedAssignments {
+				repoName := acceptedAssignment.Repository.Name
+				if len(acceptedAssignment.Students) == 1 {
+					if name, err := c.GetRepoName(acceptedAssignment.Students[0].Login); err == nil {
+						repoName = name
+					}
+				}
+				clonePath := filepath.Join(assignmentPath, repoName)
+				err := utils.CloneRepository(clonePath, acceptedAssignment.Repository.FullName, gh.Exec)
+				if err != nil {
+					errMsg := fmt.Sprintf("Error cloning %s: %v", acceptedAssignment.Repository.FullName, err)
+					cloneErrors = append(cloneErrors, errMsg)
+					continue // Continue with the next iteration
+				}
+				totalCloned++
+			}
+			if len(cloneErrors) > 0 {
+				fmt.Println("Some repositories failed to clone.")
+				if !verbose {
+					fmt.Println("Run with --verbose flag to see more details")
+				} else {
+					for _, errMsg := range cloneErrors {
+						fmt.Println(errMsg)
+					}
+				}
+			}
+			fmt.Printf("Cloned %v repos.\n", totalCloned)
+		},
+	}
+
+	cmd.Flags().IntVarP(&aId, "assignment-id", "a", 0, "ID of the assignment")
+	cmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose error output")
+
+	return cmd
+}
